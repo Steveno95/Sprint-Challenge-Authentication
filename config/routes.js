163f1b935530cc38793a16400f9206fe55af079a
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-30 10:49:49 -0600

    added a login function and a token function

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const bcrypt = require('bcryptjs');
 
 const db = require('../database/dbConfig.js');
-const { authenticate } = require('./middlewares');
+const { authenticate, generateToken } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -28,6 +28,20 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json(token);
+      } else {
+        res.status(401).json({ message: 'you shall not pass!' });
+      }
+    })
+    .catch(err => res.json(err));
 }
 
 function getJokes(req, res) {
